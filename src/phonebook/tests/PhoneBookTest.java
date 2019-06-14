4d6258476b8194f348d06462c8fa6cdaf6e6cda4
TsvetomirSholev <newcecoweee@gmail.com>
2019-06-14 13:23:51 +0300

    Done: add / remove / print sorted / validations / access number / Print top 5 outgoing calls / Fix the "Phonebook issue" / Unit tests Todo: Finished Whoops .. Final commit ????

--- src/phonebook/tests/PhoneBookTest.java
+++ src/phonebook/tests/PhoneBookTest.java
@@ -1,6 +1,12 @@
 package phonebook.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import phonebook.entities.PhoneBook;
+import phonebook.entities.PhoneBookEntry;
+import phonebook.utils.PhoneBookUtils;
+
+import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,5 +22,13 @@ class PhoneBookTest {
 
     @Test
     void addToPhonebook() {
+        File file = new File("phonebook.txt");
+        PhoneBookUtils phoneBookUtils = new PhoneBookUtils();
+        PhoneBook phonebook = phoneBookUtils.createPhoneBook(file);
+        PhoneBookEntry entry = new PhoneBookEntry("Cvetko Cvetkov Cvetkovv","0884711066");
+
+        phonebook.addToPhonebook(entry);
+
+        Assertions.assertNotNull(phonebook.getPhonebook().get("Cvetko Cvetkov Cvetkovv"));
     }
 }
--- src/phonebook/tests/PhoneBookUtilsTest.java
+++ src/phonebook/tests/PhoneBookUtilsTest.java
@@ -1,30 +1,72 @@
 package phonebook.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import phonebook.entities.PhoneBook;
+import phonebook.utils.PhoneBookUtils;
+
+import java.io.File;
 
 class PhoneBookUtilsTest {
 
     @Test
     void createPhoneBook() {
+        File file = new File("phonebook.txt");
+        PhoneBookUtils phoneBookUtils = new PhoneBookUtils();
+        PhoneBook phonebook = phoneBookUtils.createPhoneBook(file);
+
+        Assertions.assertNotNull(phonebook);
     }
 
     @Test
     void deleteByName() {
+        File file = new File("phonebook.txt");
+        PhoneBookUtils phoneBookUtils = new PhoneBookUtils();
+        PhoneBook phonebook = phoneBookUtils.createPhoneBook(file);
+
+        String name = "Васето Васков Васков";
+
+        phoneBookUtils.deleteByName("Васето Васков Васков", phonebook);
+
+        Assertions.assertNull(phonebook.getPhonebook().get(name));
     }
 
     @Test
     void addPair() {
+        File file = new File("phonebook.txt");
+        PhoneBookUtils phoneBookUtils = new PhoneBookUtils();
+        PhoneBook phonebook = phoneBookUtils.createPhoneBook(file);
+
+        String name = "Петър Петров Петровов";
+        String number = "0878123456";
+
+        phoneBookUtils.addPair(name,number , phonebook);
+
+        Assertions.assertNotNull(phonebook.getPhonebook().get(name));
     }
 
     @Test
     void getNumberByName() {
+        File file = new File("phonebook.txt");
+        PhoneBookUtils phoneBookUtils = new PhoneBookUtils();
+        PhoneBook phonebook = phoneBookUtils.createPhoneBook(file);
+
+        String name = "Васето Васков Васков";
+
+        Assertions.assertNotNull(phonebook.getPhonebook().get(name));
     }
 
     @Test
     void printEntriesSortedByName() {
+        File file = new File("phonebook.txt");
+        PhoneBookUtils phoneBookUtils = new PhoneBookUtils();
+        PhoneBook phonebook = phoneBookUtils.createPhoneBook(file);
+
+        // ?
     }
 
     @Test
     void printTopFive() {
+        // ?
     }
 }
